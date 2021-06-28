0a9bcda65fcd15aa1a5a0f074530dbbd8c8f467f
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-06-28 15:46:59 +0800

    调整控件显示权限

--- src/publicMethods/authorityJudge.ts
+++ src/publicMethods/authorityJudge.ts
@@ -1,7 +1,10 @@
 const judgeAuthority = (method: string) => {
+  debugger;
   let authFlag = false;
   const myAuth: any = localStorage.getItem("authority");
   const sys_authority: any = JSON.parse(myAuth);
+
+  console.log("myAuth", myAuth);
   for (let index = 0; index < sys_authority.length; index += 1) {
 
     if (sys_authority[index].description === method) {

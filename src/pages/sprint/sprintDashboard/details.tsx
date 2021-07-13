e317ae5dd875f0357096820be7976d50f6c220c9
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-07-13 16:40:16 +0800

    新增截止日期字段

--- src/pages/sprint/sprintDashboard/details.tsx
+++ src/pages/sprint/sprintDashboard/details.tsx
@@ -85,6 +85,11 @@ const getColums = () => {
 
     },
     {
+      headerName: '截止日期',
+      field: 'deadline',
+      cellRenderer: stageForLineThrough,
+    },
+    {
       headerName: '严重等级',
       field: 'severity',
       cellRenderer: numRenderForSevAndpriForLine,
@@ -322,6 +327,7 @@ const queryDevelopViews = async (client: GqlClient<object>, params: any) => {
           assignedTo
           finishedBy
           feedback
+          deadline
         }
       }
   `);
@@ -347,9 +353,11 @@ const queryDevelopViews = async (client: GqlClient<object>, params: any) => {
             assignedTo
             finishedBy
             feedback
+            deadline
         }
       }
   `);
+    debugger;
     return data?.overAreaList;
   }
 
@@ -379,6 +387,7 @@ const queryDevelopViews = async (client: GqlClient<object>, params: any) => {
           solveDuration
           verifyDuration
           closedDuration
+          deadline
         }
       }
       }
@@ -451,7 +460,7 @@ const DetailsList: React.FC<any> = () => {
     const [isFieldModalVisible, setFieldModalVisible] = useState(false);
     const [selectedFiled, setSelectedFiled] = useState(['']);
     const nessField = ['序号', '类型', '编号'];
-    const unNessField = ['测试', '标题内容', '严重等级', '模块', '状态', '已提测', '发布环境',
+    const unNessField = ['测试', '标题内容', '严重等级', '截止日期','模块', '状态', '已提测', '发布环境',
       '指派给', '解决/完成人', '关闭人', '备注', '相关需求', '相关任务', '相关bug', '是否可热更', '是否有数据升级',
       '是否有接口升级', '是否有预置数据', '是否需要测试验证', '验证范围建议', 'UED', 'UED测试环境验证', 'UED线上验证', '来源', '反馈人'];
 
@@ -594,6 +603,9 @@ const DetailsList: React.FC<any> = () => {
                   </Col>
                   <Col span={4}>
                     <Checkbox value="严重等级">严重等级</Checkbox>
+                  </Col>
+                  <Col span={4}>
+                    <Checkbox value="截止日期">截止日期</Checkbox>
                   </Col>
                   <Col span={4}>
                     <Checkbox value="模块">模块</Checkbox>
--- src/pages/sprint/sprintListDetails/sprintListDetails.tsx
+++ src/pages/sprint/sprintListDetails/sprintListDetails.tsx
@@ -115,7 +115,6 @@ const getColums = () => {
       minWidth: 350,
       cellRenderer: stageForLineThrough,
       tooltipField: "title"
-
     },
     {
       headerName: '严重等级',
@@ -123,6 +122,11 @@ const getColums = () => {
       cellRenderer: numRenderForSevAndpriForLine,
       minWidth: 90,
       // tooltipField: "severity"
+    },
+    {
+      headerName: '截止日期',
+      field: 'deadline',
+      cellRenderer: stageForLineThrough,
     },
     // {
     //   headerName: '优先级',
@@ -372,6 +376,7 @@ const queryDevelopViews = async (client: GqlClient<object>, prjID: any, prjType:
             relatedBugs
             relatedTasks
             relatedStories
+            deadline
           }
       }
   `);
@@ -1916,7 +1921,7 @@ const SprintList: React.FC<any> = () => {
     const [isFieldModalVisible, setFieldModalVisible] = useState(false);
     const [selectedFiled, setSelectedFiled] = useState(['']);
     const nessField = ['选择', '类型', '编号'];
-    const unNessField = ['阶段', '测试', '标题内容', '严重等级', '模块', '状态', '已提测', '发布环境',
+    const unNessField = ['阶段', '测试', '标题内容', '严重等级', '截止日期', '模块', '状态', '已提测', '发布环境',
       '指派给', '解决/完成人', '关闭人', '备注', '相关需求', '相关任务', '相关bug', '是否可热更', '是否有数据升级',
       '是否有接口升级', '是否有预置数据', '是否需要测试验证', '验证范围建议', 'UED', 'UED测试环境验证', 'UED线上验证', '来源', '反馈人'];
 
@@ -3125,6 +3130,10 @@ const SprintList: React.FC<any> = () => {
                   <Col span={4}>
                     <Checkbox value="严重等级">严重等级</Checkbox>
                   </Col>
+                  <Col span={4}>
+                    <Checkbox value="截止日期">截止日期</Checkbox>
+                  </Col>
+
                   <Col span={4}>
                     <Checkbox value="模块">模块</Checkbox>
                   </Col>

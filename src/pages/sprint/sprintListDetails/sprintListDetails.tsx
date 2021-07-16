0d44a439ae2384e3ac284c44149802637018053e
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-07-16 17:27:57 +0800

    权限测试

--- src/pages/sprint/sprintListDetails/sprintListDetails.tsx
+++ src/pages/sprint/sprintListDetails/sprintListDetails.tsx
@@ -1431,7 +1431,7 @@ debugger;
       if (initialState?.currentUser) {
         currentUserGroup = initialState.currentUser === undefined ? "" : initialState.currentUser.group;
       }
-      currentUserGroup = 'devManageGroup';
+      // currentUserGroup = 'devManageGroup';
       if (currentUserGroup !== undefined) {
         switch (currentUserGroup.toString()) {
           case 'superGroup':

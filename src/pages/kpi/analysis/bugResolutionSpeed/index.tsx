f269709bbc29db84873190df1d4ba3dbb63d4282
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-08-30 17:09:46 +0800

    新增bug解决速能功能 中查询条件

--- src/pages/kpi/analysis/bugResolutionSpeed/index.tsx
+++ src/pages/kpi/analysis/bugResolutionSpeed/index.tsx
@@ -6,15 +6,16 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import type {GridApi, GridReadyEvent} from 'ag-grid-community';
 import type {GqlClient} from '@/hooks';
-import {useGqlClient} from '@/hooks';
-import {Button, Checkbox, Col, DatePicker, Form, message, Modal, Row} from 'antd';
-import {LogoutOutlined, SettingOutlined} from '@ant-design/icons';
+import {useGqlClient, useQuery} from '@/hooks';
+import {Button, DatePicker, Form, Select} from 'antd';
+import {LogoutOutlined} from '@ant-design/icons';
 import {getHeight} from '@/publicMethods/pageSet';
 import moment from "moment";
 import {useRequest} from "ahooks";
 import dayjs from 'dayjs';
 
 const {RangePicker} = DatePicker;
+const {Option} = Select;
 
 // 格式化单元格内容
 const cellFormat = (params: any) => {
@@ -31,168 +32,43 @@ const cellFormat = (params: any) => {
 
 // 定义列名
 const getSourceColums = () => {
-
-  // 获取缓存的字段
-  const fields = localStorage.getItem("data_front_dashboard");
-
   // 定义的原始字段
   const oraFields: any = [
     {
-      headerName: "",
-      children: [
-        {
-          headerName: 'NO.',
-          minWidth: 60,
-          maxWidth: 80,
-          filter: false,
-          pinned: 'left',
-          suppressMenu: true,
-          cellRenderer: (params: any) => {
-            return Number(params.node.id) + 1;
-          },
-        },
-        {
-          headerName: '姓名',
-          field: 'userName',
-          pinned: 'left',
-          minWidth: 80,
-          suppressMenu: false,
-        },]
+      headerName: '创建日期',
+      field: 'finiStory',
+      minWidth: 105,
+      pinned: 'left',
     },
-    // {
-    //   headerName: '周期时间',
-    //   children: [
-    //     {
-    //       headerName: 'Bug解决时长(H)',
-    //       field: 'maxLines',
-    //       minWidth: 133,
-    //       valueFormatter: cellFormat
-    //     },
-    //     {
-    //       headerName: 'Bug数量',
-    //       field: 'avgLines',
-    //       minWidth: 88,
-    //     }
-    //   ]
-    // },
-    // {
-    //   headerName: '',
-    //   children: [{
-    //     headerName: '任务燃尽图',
-    //     field: 'minLines',
-    //     minWidth: 120,
-    //   }]
-    // },
-    // {
-    //   headerName: '速度',
-    //   children: [
-    //     {
-    //       headerName: '初始需求数',
-    //       field: 'deptName',
-    //       minWidth: 105,
-    //     },
-    //     {
-    //       headerName: '初始需求完成数',
-    //       field: 'groupName',
-    //       minWidth: 130,
-    //     },
-    //     {
-    //       headerName: '追加需求数',
-    //       field: 'tech',
-    //       minWidth: 105,
-    //     },
-    //     {
-    //       headerName: '追加需求完成数',
-    //       field: 'area',
-    //       minWidth: 130,
-    //     }
-    //   ]
-    // },
-    // {
-    //   headerName: '对外请求',
-    //   children: [{
-    //     headerName: '请求数',
-    //     field: 'position',
-    //     minWidth: 85,
-    //   },
-    //     {
-    //       headerName: '请求平均停留时长',
-    //       field: 'job',
-    //       minWidth: 140,
-    //     }]
-    // },
     {
-      headerName: '吞吐量',
-      children: [
-        {
-          headerName: '交付需求数',
-          field: 'finiStory',
-          minWidth: 105,
-          valueFormatter: cellFormat
-        },
-        {
-          headerName: '完成任务数',
-          field: 'finiTask',
-          minWidth: 105,
-          valueFormatter: cellFormat
-        },
-        {
-          headerName: '修复Bug数',
-          field: 'resolvedBug',
-          minWidth: 105,
-          valueFormatter: cellFormat
-        },
-        {
-          headerName: '进行中任务数',
-          field: 'doingTask',
-          minWidth: 115,
-          valueFormatter: cellFormat
-        },
-        {
-          headerName: '代码提交次数',
-          field: 'codeCommit',
-          minWidth: 115,
-          valueFormatter: cellFormat
-        },
-        {
-          headerName: '代码新增行数',
-          field: 'newLine',
-          minWidth: 115,
-          valueFormatter: cellFormat
-        }
-      ]
+      headerName: '新增',
+      field: 'finiStory',
+      minWidth: 80,
+      pinned: 'left',
+    },
+    {
+      headerName: '状态',
+      field: 'finiStory',
+      minWidth: 80,
+      pinned: 'left',
+    },
+    {
+      headerName: '级别',
+      field: 'finiStory',
+      minWidth: 80,
+      pinned: 'left',
+    },
+    {
+      headerName: '初始',
+      field: 'finiStory',
+      minWidth: 80,
+      pinned: 'left',
     }
 
   ];
 
-  if (fields === null) {
-    return oraFields;
-  }
 
-  const selected = JSON.parse(fields);
-
-  const component: any = [];
-  oraFields.forEach((parent: any) => {
-    const p_details = parent.children;
-    const childs: any = [];
-    p_details.forEach((c_details: any) => {
-      const newElement = c_details;
-      if (selected.includes(c_details.headerName)) {
-        newElement.hide = false;
-      } else {
-        newElement.hide = true;
-      }
-      childs.push(newElement);
-    });
-
-    component.push({
-      headerName: parent.headerName,
-      children: childs,
-    })
-
-  });
-
-  return component;
+  return oraFields;
 };
 
 // 公共查询方法
@@ -200,13 +76,7 @@ const queryFrontData = async (client: GqlClient<object>, params: any) => {
 
   const {data} = await client.query(`{
           dashFront(start:"${params.start}",end:"${params.end}"){
-            userName
-            finiStory
-            finiTask
-            resolvedBug
-            doingTask
-            codeCommit
-            newLine
+
           }
 
       }
@@ -216,10 +86,28 @@ const queryFrontData = async (client: GqlClient<object>, params: any) => {
   return data?.dashFront;
 };
 
+const GetSprintProject = () => {
+  const projectArray = [];
+
+  const {data: {project = []} = {}} = useQuery(`{
+        project(range:{start:"", end:""},,order:DESC){
+        id
+        name
+      }
+    }`);
+
+  for (let index = 0; index < project.length; index += 1) {
+    projectArray.push(
+      <Option key={project[index].id.toString()} value={project[index].id.toString()}> {project[index].name}</Option>,
+    );
+  }
+  return projectArray;
+};
+
 const FrontTableList: React.FC<any> = () => {
   const g_currentMonth_range = {
-    start: dayjs().startOf('month').format("YYYY-MM-DD"),
-    end: dayjs().endOf('month').format("YYYY-MM-DD")
+    start: dayjs().startOf('week').format("YYYY-MM-DD"),
+    end: dayjs().endOf('week').format("YYYY-MM-DD")
   };
 
   const gqlClient = useGqlClient();
@@ -242,14 +130,16 @@ const FrontTableList: React.FC<any> = () => {
     gridApiForFront.current?.sizeColumnsToFit();
   };
 
-  const [choicedConditionForSource, setQueryConditionForSource] = useState({
+  const [choicedCondition, setQueryConditionForSource] = useState({
+    prjName: [],
     start: g_currentMonth_range.start,
     end: g_currentMonth_range.end
   });
 
-  //  默认显示本月数据（1号-31号）
+  //  默认显示
   const showSourceDefaultData = async () => {
     setQueryConditionForSource({
+      prjName: [],
       start: g_currentMonth_range.start,
       end: g_currentMonth_range.end
     });
@@ -261,11 +151,13 @@ const FrontTableList: React.FC<any> = () => {
   const onSourceTimeSelected = async (params: any, dateString: any) => {
 
     setQueryConditionForSource({
+      ...choicedCondition,
       start: dateString[0],
       end: dateString[1]
     });
 
     const range = {
+      prjNames: choicedCondition.prjName,
       start: dateString[0],
       end: dateString[1]
     };
@@ -275,59 +167,19 @@ const FrontTableList: React.FC<any> = () => {
 
   };
 
-  /* region 显示自定义字段 */
-  const [isFieldModalVisible, setFieldModalVisible] = useState(false);
-  const [selectedFiled, setSelectedFiled] = useState(['']);
-  const nessField = ['NO.', '姓名'];
-  const unNessField = ['Bug解决时长(H)', 'Bug数量', '任务燃尽图', '初始需求数', '初始需求完成数', '追加需求数', '追加需求完成数',
-    '请求数', '请求平均停留时长', '交付需求数', '完成任务数', '修复Bug数', '进行中任务数', '代码提交次数', '代码新增行数'];
+  // 项目名称选择事件
+  const prjNameChanged = async (value: any, params: any) => {
 
-// 弹出字段显示层
-  const showFieldsModal = () => {
-    const fields = localStorage.getItem("data_front_dashboard");
-    if (fields === null) {
-      setSelectedFiled(nessField.concat(unNessField));
-    } else {
-      setSelectedFiled(JSON.parse(fields));
-    }
-    setFieldModalVisible(true);
-  };
+    const range = {
+      prjNames: value,
+      start: choicedCondition.start,
+      end: choicedCondition.end
+    };
 
-// 全选
-  const selectAllField = (e: any) => {
-    if (e.target.checked === true) {
-      setSelectedFiled(nessField.concat(unNessField));
-    } else {
-      setSelectedFiled(nessField);
-    }
-  };
-
-// 保存按钮
-  const commitField = () => {
-    localStorage.setItem("data_front_dashboard", JSON.stringify(selectedFiled));
-    setFieldModalVisible(false);
-    // 首先需要清空原有列，否则会导致列混乱
-    gridApiForFront.current?.setColumnDefs([]);
-
-    message.info({
-      content: "保存成功！",
-      duration: 1,
-      style: {
-        marginTop: '50vh',
-      },
-    });
+    const datas: any = await queryFrontData(gqlClient, range);
+    gridApiForFront.current?.setRowData(datas);
 
   };
-// 取消
-  const fieldCancel = () => {
-    setFieldModalVisible(false);
-  };
-
-// 缓存到state
-  const onSetFieldsChange = (checkedValues: any) => {
-    setSelectedFiled(checkedValues);
-  };
-  /* endregion */
 
 
   return (
@@ -335,24 +187,27 @@ const FrontTableList: React.FC<any> = () => {
 
       <div>
         {/* 查询条件 */}
-        <div style={{width: '100%', height: 45, marginTop: 15, backgroundColor: "white"}}>
+        <div style={{width: '100%', height: 45, marginTop: 5, backgroundColor: "white"}}>
           <Form.Item>
 
-            <label style={{marginLeft: "10px", marginTop: 7}}>查询周期：</label>
+            <label style={{marginLeft: '10px'}}>项目名称：</label>
+            <Select placeholder="请选择" style={{width: '20%'}} mode="multiple" maxTagCount={'responsive'} showSearch
+                    optionFilterProp="children"
+                    onChange={prjNameChanged}>
+              {GetSprintProject()}
+            </Select>
+
+            <label style={{marginLeft: "20px", marginTop: 7}}>查询周期：</label>
             <RangePicker
-              style={{width: '30%', marginTop: 7}} onChange={onSourceTimeSelected}
-              value={[choicedConditionForSource.start === "" ? null : moment(choicedConditionForSource.start),
-                choicedConditionForSource.end === "" ? null : moment(choicedConditionForSource.end)]}
+              style={{width: '20%', marginTop: 7}} onChange={onSourceTimeSelected}
+              value={[choicedCondition.start === "" ? null : moment(choicedCondition.start),
+                choicedCondition.end === "" ? null : moment(choicedCondition.end)]}
             />
 
             <Button type="text" style={{marginLeft: "20px", color: 'black'}}
                     icon={<LogoutOutlined/>} size={'small'} onClick={showSourceDefaultData}>
               默认：</Button>
-            <label style={{marginLeft: "-10px", color: 'black'}}> 默认1个月</label>
-
-            <Button type="text" icon={<SettingOutlined/>} size={'large'} onClick={showFieldsModal}
-                    style={{float: "right", marginTop: 5}}> </Button>
-
+            <label style={{marginLeft: "-10px", color: 'black'}}> 默认1周</label>
 
           </Form.Item>
 
@@ -388,85 +243,6 @@ const FrontTableList: React.FC<any> = () => {
           </AgGridReact>
         </div>
 
-        {/* 自定义字段 */}
-        <Modal
-          title={'自定义字段'}
-          visible={isFieldModalVisible}
-          onCancel={fieldCancel}
-          centered={true}
-          footer={null}
-          width={920}
-        >
-          <Form>
-            <div>
-              <Checkbox.Group style={{width: '100%'}} value={selectedFiled} onChange={onSetFieldsChange}>
-                <Row>
-                  <Col span={4}>
-                    <Checkbox defaultChecked disabled value="NO.">NO.</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox defaultChecked disabled value="姓名">姓名</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="Bug解决时长(H)">Bug解决时长</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="Bug数量">Bug数量</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="任务燃尽图">任务燃尽图</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="初始需求数">初始需求数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="初始需求完成数">初始需求完成数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="追加需求数">追加需求数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="追加需求完成数">追加需求完成数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="请求数">请求数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="请求平均停留时长">请求平均停留时长</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="交付需求数">交付需求数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="完成任务数">完成任务数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="修复Bug数">修复Bug数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="进行中任务数">进行中任务数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="代码提交次数">代码提交次数</Checkbox>
-                  </Col>
-                  <Col span={4}>
-                    <Checkbox value="代码新增行数">代码新增行数</Checkbox>
-                  </Col>
-                </Row>
-              </Checkbox.Group>,
-            </div>
-
-            <div>
-              <Checkbox onChange={selectAllField}>全选</Checkbox>
-
-              <Button type="primary" style={{marginLeft: '300px'}} onClick={commitField}>
-                确定</Button>
-              <Button type="primary" style={{marginLeft: '20px'}} onClick={fieldCancel}>
-                取消</Button>
-            </div>
-
-          </Form>
-        </Modal>
       </div>
 
     </PageContainer>

423d5f50437da2a8ad2919e62aea2992b6159653
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-11-04 17:10:59 +0800

    开发千行bug率按年统计

--- src/pages/kpi/performance/developer/thousandBugsRate/index.tsx
+++ src/pages/kpi/performance/developer/thousandBugsRate/index.tsx
@@ -12,11 +12,17 @@ import {
   getMonthWeek,
   getTwelveMonthTime,
   getFourQuarterTime,
-  getParamsByType
+  getParamsByType, getYearsTime
 } from '@/publicMethods/timeMethods';
 import {colorRender, moduleChange} from '@/publicMethods/cellRenderer';
 import {Button, Drawer} from "antd";
-import {ScheduleTwoTone, CalendarTwoTone, ProfileTwoTone, QuestionCircleTwoTone} from "@ant-design/icons";
+import {
+  ScheduleTwoTone,
+  CalendarTwoTone,
+  ProfileTwoTone,
+  QuestionCircleTwoTone,
+  AppstoreTwoTone
+} from "@ant-design/icons";
 import {getHeight} from "@/publicMethods/pageSet";
 
 
@@ -126,6 +132,20 @@ const columsForQuarters = () => {
   return compColums.concat(component);
 };
 
+const columsForYears = () => {
+  const yearsTime = getYearsTime();
+  const component = new Array();
+  for (let index = 0; index < yearsTime.length; index += 1) {
+    component.push({
+      headerName: yearsTime[index].title,
+      field: yearsTime[index].start,
+      aggFunc: codeNumberRender,
+      cellRenderer: colorRender
+    });
+
+  }
+  return compColums.concat(component);
+};
 /* endregion */
 
 /* region 数据处理 */
@@ -397,6 +417,15 @@ const BugRateTableList: React.FC<any> = () => {
     gridApi.current?.setRowData(datas);
   };
 
+  // 按年统计
+  const statisticsByYear = async () => {
+    gridApi.current?.setColumnDefs([]);
+    const quartersColums = columsForYears();
+    gridApi.current?.setColumnDefs(quartersColums);
+    const datas: any = await queryBugResolutionCount(gqlClient, 'year');
+    gridApi.current?.setRowData(datas);
+  };
+
   /* region 提示规则显示 */
   const [messageVisible, setVisible] = useState(false);
   const showRules = () => {
@@ -418,6 +447,8 @@ const BugRateTableList: React.FC<any> = () => {
                 onClick={statisticsByMonths}>按月统计</Button>
         <Button type="text" style={{color: 'black'}} icon={<ScheduleTwoTone/>} size={'large'}
                 onClick={statisticsByQuarters}>按季统计</Button>
+        <Button type="text" style={{color: 'black'}} icon={<AppstoreTwoTone/>} size={'large'}
+                onClick={statisticsByYear}>按年统计</Button>
         <Button type="text" style={{color: '#1890FF', float: 'right'}} icon={<QuestionCircleTwoTone/>}
                 size={'large'} onClick={showRules}>计算规则</Button>
       </div>

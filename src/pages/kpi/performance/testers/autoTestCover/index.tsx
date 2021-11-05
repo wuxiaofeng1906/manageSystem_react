4e87e2a1f2838c7d639c5ae26697afe2e97ff3af
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-11-05 16:10:23 +0800

    测试自动化覆盖率:提供按年统计的数据

--- src/pages/kpi/performance/testers/autoTestCover/index.tsx
+++ src/pages/kpi/performance/testers/autoTestCover/index.tsx
@@ -12,10 +12,16 @@ import {
   getMonthWeek,
   getTwelveMonthTime,
   getFourQuarterTime,
-  getParamsByType
+  getParamsByType, getYearsTime
 } from '@/publicMethods/timeMethods';
 import {Button, Drawer} from "antd";
-import {ScheduleTwoTone, CalendarTwoTone, ProfileTwoTone, QuestionCircleTwoTone} from "@ant-design/icons";
+import {
+  ScheduleTwoTone,
+  CalendarTwoTone,
+  ProfileTwoTone,
+  QuestionCircleTwoTone,
+  AppstoreTwoTone
+} from "@ant-design/icons";
 import {getHeight} from "@/publicMethods/pageSet";
 
 
@@ -122,6 +128,20 @@ const columsForQuarters = () => {
   return compColums.concat(component);
 };
 
+const columsForYears = () => {
+  const yearsTime = getYearsTime();
+  const component = new Array();
+  for (let index = 0; index < yearsTime.length; index += 1) {
+    component.push({
+      headerName: yearsTime[index].title,
+      field: yearsTime[index].start,
+      aggFunc: codeNumberRender,
+      cellRenderer: colorRender
+    });
+
+  }
+  return compColums.concat(component);
+};
 /* endregion */
 
 /* region 数据处理 */
@@ -227,7 +247,7 @@ const converseArrayToOne = (data: any) => {
   return resultData;
 };
 
-const queryBugResolutionCount = async (client: GqlClient<object>, params: string) => {
+const queryCoverageRate = async (client: GqlClient<object>, params: string) => {
   const condition = getParamsByType(params);
   if (condition.typeFlag === 0) {
     return [];
@@ -278,7 +298,7 @@ const BugReturnTableList: React.FC<any> = () => {
   const gridApi = useRef<GridApi>();
   const gqlClient = useGqlClient();
   const {data, loading} = useRequest(() =>
-    queryBugResolutionCount(gqlClient, 'quarter'),
+    queryCoverageRate(gqlClient, 'quarter'),
   );
   const onGridReady = (params: GridReadyEvent) => {
     gridApi.current = params.api;
@@ -304,7 +324,7 @@ const BugReturnTableList: React.FC<any> = () => {
     gridApi.current?.setColumnDefs([]);
     const weekColums = columsForWeeks();
     gridApi.current?.setColumnDefs(weekColums);
-    const datas: any = await queryBugResolutionCount(gqlClient, 'week');
+    const datas: any = await queryCoverageRate(gqlClient, 'week');
     gridApi.current?.setRowData(datas);
 
   };
@@ -315,7 +335,7 @@ const BugReturnTableList: React.FC<any> = () => {
     gridApi.current?.setColumnDefs([]);
     const monthColums = columsForMonths();
     gridApi.current?.setColumnDefs(monthColums);
-    const datas: any = await queryBugResolutionCount(gqlClient, 'month');
+    const datas: any = await queryCoverageRate(gqlClient, 'month');
     gridApi.current?.setRowData(datas);
 
   };
@@ -326,7 +346,16 @@ const BugReturnTableList: React.FC<any> = () => {
     gridApi.current?.setColumnDefs([]);
     const quartersColums = columsForQuarters();
     gridApi.current?.setColumnDefs(quartersColums);
-    const datas: any = await queryBugResolutionCount(gqlClient, 'quarter');
+    const datas: any = await queryCoverageRate(gqlClient, 'quarter');
+    gridApi.current?.setRowData(datas);
+  };
+
+  // 按年统计
+  const statisticsByYear = async () => {
+    gridApi.current?.setColumnDefs([]);
+    const yearColums = columsForYears();
+    gridApi.current?.setColumnDefs(yearColums);
+    const datas: any = await queryCoverageRate(gqlClient, 'year');
     gridApi.current?.setRowData(datas);
   };
 
@@ -351,6 +380,9 @@ const BugReturnTableList: React.FC<any> = () => {
                 onClick={statisticsByMonths}>按月统计</Button>
         <Button type="text" style={{color: 'black'}} icon={<ScheduleTwoTone/>} size={'large'}
                 onClick={statisticsByQuarters}>按季统计</Button>
+
+        <Button type="text" style={{color: 'black'}} icon={<AppstoreTwoTone/>} size={'large'}
+                onClick={statisticsByYear}>按年统计</Button>
         <label style={{fontWeight: "bold"}}>(统计单位：%)</label>
 
         <Button type="text" style={{color: '#1890FF', float: 'right'}} icon={<QuestionCircleTwoTone/>}

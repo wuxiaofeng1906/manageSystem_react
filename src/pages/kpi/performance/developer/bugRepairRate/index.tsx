43875b2d74f5d106e42e41408fca97c162f10209
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-11-05 14:30:04 +0800

    bug修复率:提供按年统计的数据

--- src/pages/kpi/performance/developer/bugRepairRate/index.tsx
+++ src/pages/kpi/performance/developer/bugRepairRate/index.tsx
@@ -12,11 +12,17 @@ import {
   getMonthWeek,
   getTwelveMonthTime,
   getFourQuarterTime,
-  getParamsByType
+  getParamsByType, getYearsTime
 } from '@/publicMethods/timeMethods';
 import {moduleChange} from '@/publicMethods/cellRenderer';
 import {Button, Drawer} from "antd";
-import {ScheduleTwoTone, CalendarTwoTone, ProfileTwoTone, QuestionCircleTwoTone} from "@ant-design/icons";
+import {
+  ScheduleTwoTone,
+  CalendarTwoTone,
+  ProfileTwoTone,
+  QuestionCircleTwoTone,
+  AppstoreTwoTone
+} from "@ant-design/icons";
 import {customRound, getHeight} from "@/publicMethods/pageSet";
 
 
@@ -175,6 +181,21 @@ const columsForQuarters = () => {
   return compColums.concat(component);
 };
 
+
+const columsForYears = () => {
+  const yearsTime = getYearsTime();
+  const component = new Array();
+  for (let index = 0; index < yearsTime.length; index += 1) {
+    component.push({
+      headerName: yearsTime[index].title,
+      field: yearsTime[index].start,
+      aggFunc: codeNumberRender,
+      cellRenderer: colorRender
+    });
+
+  }
+  return compColums.concat(component);
+};
 /* endregion */
 
 /* region 数据处理 */
@@ -333,7 +354,7 @@ const converseArrayToOne = (data: any) => {
 };
 
 
-const queryBugResolutionCount = async (client: GqlClient<object>, params: string) => {
+const queryBugRepaireRate = async (client: GqlClient<object>, params: string) => {
   const condition = getParamsByType(params, true);
   if (condition.typeFlag === 0) {
     return [];
@@ -388,7 +409,7 @@ const BugReopenTableList: React.FC<any> = () => {
   const gridApi = useRef<GridApi>();
   const gqlClient = useGqlClient();
   const {data, loading} = useRequest(() =>
-    queryBugResolutionCount(gqlClient, 'quarter'),
+    queryBugRepaireRate(gqlClient, 'quarter'),
   );
   const onGridReady = (params: GridReadyEvent) => {
     gridApi.current = params.api;
@@ -415,7 +436,7 @@ const BugReopenTableList: React.FC<any> = () => {
     gridApi.current?.setColumnDefs([]);
     const weekColums = columsForWeeks();
     gridApi.current?.setColumnDefs(weekColums);
-    const datas: any = await queryBugResolutionCount(gqlClient, 'week');
+    const datas: any = await queryBugRepaireRate(gqlClient, 'week');
     gridApi.current?.setRowData(datas);
 
   };
@@ -426,7 +447,7 @@ const BugReopenTableList: React.FC<any> = () => {
     gridApi.current?.setColumnDefs([]);
     const monthColums = columsForMonths();
     gridApi.current?.setColumnDefs(monthColums);
-    const datas: any = await queryBugResolutionCount(gqlClient, 'month');
+    const datas: any = await queryBugRepaireRate(gqlClient, 'month');
     gridApi.current?.setRowData(datas);
 
   };
@@ -437,7 +458,16 @@ const BugReopenTableList: React.FC<any> = () => {
     gridApi.current?.setColumnDefs([]);
     const quartersColums = columsForQuarters();
     gridApi.current?.setColumnDefs(quartersColums);
-    const datas: any = await queryBugResolutionCount(gqlClient, 'quarter');
+    const datas: any = await queryBugRepaireRate(gqlClient, 'quarter');
+    gridApi.current?.setRowData(datas);
+  };
+
+  // 按年统计
+  const statisticsByYear = async () => {
+    gridApi.current?.setColumnDefs([]);
+    const yearsColums = columsForYears();
+    gridApi.current?.setColumnDefs(yearsColums);
+    const datas: any = await queryBugRepaireRate(gqlClient, 'year');
     gridApi.current?.setRowData(datas);
   };
 
@@ -462,6 +492,8 @@ const BugReopenTableList: React.FC<any> = () => {
                 onClick={statisticsByMonths}>按月统计</Button>
         <Button type="text" style={{color: 'black'}} icon={<ScheduleTwoTone/>} size={'large'}
                 onClick={statisticsByQuarters}>按季统计</Button>
+        <Button type="text" style={{color: 'black'}} icon={<AppstoreTwoTone/>} size={'large'}
+                onClick={statisticsByYear}>按年统计</Button>
         <label style={{fontWeight: "bold"}}>(统计单位：%)</label>
 
         <Button type="text" style={{color: '#1890FF', float: 'right'}} icon={<QuestionCircleTwoTone/>}

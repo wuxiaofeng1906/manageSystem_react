0634142ba8f730c884e81c9c61dd4858e10accef
jie.tan <jie.tan@77hub.com>
2021-11-26 14:44:01 +0800

    存档

--- src/pages/kpi/forProject/ProcessQuality/TableList/index.tsx
+++ src/pages/kpi/forProject/ProcessQuality/TableList/index.tsx
@@ -2,14 +2,14 @@
  * @Description: 数据列表
  * @Author: jieTan
  * @Date: 2021-11-22 10:55:42
- * @LastEditTime: 2021-11-23 17:04:20
+ * @LastEditTime: 2021-11-23 17:41:41
  * @LastEditors: jieTan
  * @LastModify:
  */
 
 import './index.css';
 import { useGqlClient } from '@/hooks';
-import { HOUR, NUMBER_W, PERCENTAGE } from '@/namespaces';
+import { DEFAULT_PLACEHOLDER, HOUR, NUMBER_W, PERCENTAGE } from '@/namespaces';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import { useRequest } from 'ahooks';
 import { GQL_PARAMS, queryGQL } from '../../gql.query';
@@ -21,7 +21,7 @@ import BugResolveDuraColumn from './renders/BugResolveDuraColumn';
 import BugFlybackDuraColumn from './renders/BugFlybackDuraColumn';
 
 /*  */
-const EmptyUi = () => '-';
+const EmptyUi = () => DEFAULT_PLACEHOLDER;
 
 /*  */
 export default () => {
@@ -42,7 +42,7 @@ export default () => {
 
   /*  */
   return (
-    <div className="ag-theme-material" style={{ height: 200 }}>
+    <div className="ag-theme-material" style={{ height: 400 }}>
       <AgGridReact
         frameworkComponents={{
           project: ProjectColumn,
@@ -67,8 +67,18 @@ export default () => {
           cellRenderer="bugNumber"
           {...doubleNumberW}
         />
-        <AgGridColumn headerName="千行Bug率" field="thouslineRatio" valueGetter={EmptyUi}  {...doubleNumberW} />
-        <AgGridColumn headerName="单元覆盖率" field="unitCover" valueGetter={EmptyUi}  {...doubleNumberW} />
+        <AgGridColumn
+          headerName="千行Bug率"
+          field="thouslineRatio"
+          valueGetter={EmptyUi}
+          {...doubleNumberW}
+        />
+        <AgGridColumn
+          headerName="单元覆盖率"
+          field="unitCover"
+          valueGetter={EmptyUi}
+          {...doubleNumberW}
+        />
         <AgGridColumn
           headerName={`ReOpen率(${PERCENTAGE['unit']})`}
           field="reopenRatio"
@@ -83,7 +93,12 @@ export default () => {
           cellRendererParams={{ delta: HOUR['value'] }}
           {...doubleNumberW}
         />
-        <AgGridColumn headerName="有效Bug率" field="effectiveBugRatio" valueGetter={EmptyUi}  {...ratioW} />
+        <AgGridColumn
+          headerName="有效Bug率"
+          field="effectiveBugRatio"
+          valueGetter={EmptyUi}
+          {...ratioW}
+        />
         <AgGridColumn
           headerName={`回归时长(${HOUR['unit']})`}
           field="bugFlybackDura"
@@ -92,7 +107,7 @@ export default () => {
           {...doubleNumberW}
         />
         <AgGridColumn headerName="用例数" field="caseNumber" {...doubleNumberW} />
-        <AgGridColumn headerName="自动化测试覆盖率" field="autoCoverRatio" valueGetter={EmptyUi}  />
+        <AgGridColumn headerName="自动化测试覆盖率" field="autoCoverRatio" valueGetter={EmptyUi} />
       </AgGridReact>
     </div>
   );

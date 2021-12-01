e9766b7443bdd192e58634a3178303f78c06582b
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-12-01 18:25:52 +0800

    ip 地址修改 2

--- config/ip.ts
+++ config/ip.ts
@@ -3,7 +3,7 @@ const getCurrentProxy = () => {
 
   const url = window.location.host;
 
-  if (url === "http://dms.q7link.com:8000/") {
+  if (url === "dms.q7link.com:8000") {
     return "http://10.0.144.51:5000/";
 
   } else {

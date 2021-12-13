e5000f91a7d863fc7722182500893e825dce04dd
wuxiaofeng <xiaofeng.wu@77hub.com>
2021-12-13 14:47:16 +0800

    新增'frontManager'权限

--- src/access.ts
+++ src/access.ts
@@ -8,10 +8,11 @@ export default function access(initialState: { currentUser?: API.CurrentUser | u
     devCenter: currentUser && currentUser.access !== 'service',
     // 上线前检查：超级管理员、开发经理/总监、开发人员组、测试人员组、测试经理 拥有权限
     onlineCheck: currentUser && (currentUser.access === 'superGroup' || currentUser.access === 'devManageGroup' ||
-      currentUser.access === 'devGroup' || currentUser.access === 'testGroup' || currentUser.access === 'projectListMG'),
+      currentUser.access === 'devGroup' || currentUser.access === 'testGroup' || currentUser.access === 'projectListMG'
+      || currentUser.access === 'frontManager'),
     // sonar扫描：超级管理员、开发经理/总监、开发人员组
     sonarCheck: currentUser && (currentUser.access === 'superGroup' || currentUser.access === 'devManageGroup' ||
-      currentUser.access === 'devGroup'),
+      currentUser.access === 'devGroup' || currentUser.access === 'frontManager'),
     frontManager: currentUser && (currentUser.access === 'superGroup' || currentUser.access === 'frontManager')
   };
 }

98149014d294064eeded1a459cea0def8eb5ee28
little <jie.tan@77hub.com>
2022-01-07 06:31:21 +0000

    pk：服务的字段：显示的width更新

--- src/pages/kpi/forProject/ProjectMetric/TableList/definitions/columns/ServiceAboutCols.ts
+++ src/pages/kpi/forProject/ProjectMetric/TableList/definitions/columns/ServiceAboutCols.ts
@@ -2,29 +2,26 @@
  * @Description: 服务的字段
  * @Author: jieTan
  * @Date: 2021-11-29 15:47:07
- * @LastEditTime: 2022-01-07 02:14:06
+ * @LastEditTime: 2022-01-07 06:29:13
  * @LastEditors: jieTan
  * @LastModify:
  */
 
-import { PROJ_METRIC } from '@/namespaces';
+import { PERCENTAGE, PROJ_METRIC } from '@/namespaces';
 import { ColDef, ColGroupDef } from 'ag-grid-community';
-import { ratioW } from './baseParams';
 
 /*  */
 const moduleName = PROJ_METRIC.serviceAbout.en;
-const defaultParmas = {
-  cellRenderer: 'numToFixed',
-  cellRendererParams: { decimal: 4 },
-  ...ratioW,
-};
 
 /* 需求稳定性 */
 export const ServiceAboutCols: ColDef[] = [
   {
-    headerName: '一次发布成功率',
+    headerName: `一次发布成功率(${PERCENTAGE['unit']})`,
     field: `${moduleName}.releaseSucc`,
-    ...defaultParmas,
+    cellRenderer: 'numToFixed',
+    cellRendererParams: { multiple: PERCENTAGE.value },
+    width: 156,
+    minWidth: 150,
   },
 ];
 //

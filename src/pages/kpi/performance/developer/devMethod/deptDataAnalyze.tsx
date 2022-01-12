7b200d1d4b3a2c1fafcc5e2cc74b941590cae29e
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-01-12 15:52:11 +0800

    单元测试覆盖率按组统计

--- src/pages/kpi/performance/developer/devMethod/deptDataAnalyze.tsx
+++ src/pages/kpi/performance/developer/devMethod/deptDataAnalyze.tsx
@@ -150,5 +150,114 @@ const converseFormatForAgGrid = (oraDatas: any) => {
   return converseArrayToOne(resultArray);
 };
 
+// 转化为ag-grid能被显示的格式
+const converseCoverageFormatForAgGrid = (oraDatas: any) => {
+  if (!oraDatas) {
+    return [];
+  }
 
-export {converseFormatForAgGrid};
+  const resultArray: any = [];
+
+  // 解析部门数据
+  oraDatas.forEach((elements: any) => {
+
+    const endtime = elements.range.end;
+
+    // 新增研发中心数据
+    resultArray.push({
+      Group: ['研发中心'],
+      [`instCove${endtime}`]: elements.total.instCove,
+      [`branCove${endtime}`]: elements.total.branCove,
+      isDept: true
+    }, {
+      Group: ['研发中心', '前端'],
+      [`instCove${endtime}`]: elements.side.front.instCove,
+      [`branCove${endtime}`]: elements.side.front.branCove,
+      isDept: true
+    }, {
+      Group: ['研发中心', '后端'],
+      [`instCove${endtime}`]: elements.side.backend.instCove,
+      [`branCove${endtime}`]: elements.side.backend.branCove,
+      isDept: true
+    });
+
+    const departDatas = elements.datas;
+
+    // 部门数据
+    departDatas.forEach((depts: any) => {
+      debugger;
+
+      /* region 部门数据 */
+
+      const groups: any = [depts.deptName];
+      findParent(departDatas, depts, groups);
+
+      // 新增部门
+      resultArray.push({
+        Group: groups,
+        [`instCove${endtime}`]: depts.instCove,
+        [`branCove${endtime}`]: depts.branCove,
+        isDept: true
+      });
+
+      // 需要判断部门有没有前后端：
+
+      // 没有前端：应用架构部==>如果不是应用架构部，就添加前端数据
+      if (depts.deptName !== "应用架构部") {
+        // 新增部门的前端
+        const frontGroup: any = JSON.parse(JSON.stringify(groups)); // 如果对原数组groups进行直接赋值，再对frontGroup进行修改，groups也会被修改（数组所指向的是内存地址，直接赋值会使它们指向同一地址）
+        frontGroup.push("前端")
+        resultArray.push({
+          Group: frontGroup,
+          [`instCove${endtime}`]: depts.side.front.instCove,
+          [`branCove${endtime}`]: depts.side.front.branCove,
+          isDept: true
+        });
+      }
+
+      // 没有后端：前端应用平台，基础技术=》如果不是前端应用平台和基础技术就添加后端数据
+      if (depts.deptName !== "前端应用平台" && depts.deptName !== "基础技术") {
+        // 新增部门的后端
+        const backendGroup: any = JSON.parse(JSON.stringify(groups));
+        backendGroup.push("后端")
+        resultArray.push({
+          Group: backendGroup,
+          [`instCove${endtime}`]: depts.side.backend.instCove,
+          [`branCove${endtime}`]: depts.side.backend.branCove,
+          isDept: true
+        });
+      }
+      /* endregion 部门数据 */
+
+      /* region 人员数据 */
+
+      const usersArray = depts.users;
+      if (usersArray) {
+        usersArray.forEach((user: any) => {
+
+          const usersGroup = JSON.parse(JSON.stringify(groups));
+          if (user.tech === "1") {
+            usersGroup.push("前端");
+          } else if (user.tech === "2") {
+            usersGroup.push("后端");
+          }
+          usersGroup.push(user.userName);
+          resultArray.push({
+            Group: usersGroup,
+            [`instCove${endtime}`]: user.instCove,
+            [`branCove${endtime}`]: user.branCove,
+            isDept: false
+          });
+        });
+      }
+
+      /* endregion 人员数据 */
+    });
+
+  });
+
+
+  return converseArrayToOne(resultArray);
+};
+
+export {converseFormatForAgGrid, converseCoverageFormatForAgGrid};

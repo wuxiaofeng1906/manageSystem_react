1dead2d333adafbd21ff71e9826e36e22332d3cb
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-03-31 18:36:13 +0800

    新增页头

--- src/pages/shimo/fileBaseline/iterateList/components/PageHeader/index.tsx
+++ src/pages/shimo/fileBaseline/iterateList/components/PageHeader/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import {PageContainer} from "@ant-design/pro-layout";
+import {Button} from 'antd';
 
 const PageHeader: React.FC<any> = () => {
 
+
   return (
-
-    <div>
-      title展示以及去重按钮
-    </div>
-
+    <PageContainer
+      extra={[
+        <Button type="primary"
+                style={{
+                  float: "right", color: '#46A0FC',
+                  backgroundColor: "#ECF5FF", borderRadius: 5, marginLeft: 20
+                }}>点击生成
+        </Button>
+      ]}
+    />
   );
-
 }
+
 
 export default PageHeader

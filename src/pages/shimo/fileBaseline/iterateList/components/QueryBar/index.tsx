a7fc65c07c9cca49864dbb64363dbd315d31bd8f
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-04-02 18:13:10 +0800

    修改默认时间

--- src/pages/shimo/fileBaseline/iterateList/components/QueryBar/index.tsx
+++ src/pages/shimo/fileBaseline/iterateList/components/QueryBar/index.tsx
@@ -24,7 +24,7 @@ const QueryBar: React.FC<any> = () => {
       dept: "",
       iterName: "",
       SQA: "",
-      iterRange: [moment(dayjs().startOf("year").format("YYYYMMDD")), moment(dayjs().format("YYYYMMDD"))]
+      iterRange: [moment(dayjs().startOf("year").format("YYYYMMDD")), moment(dayjs().endOf("year").format("YYYYMMDD"))]
     });
   });
   return (

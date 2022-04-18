1d41c28e270b7192030042c358e654bc495f2052
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-04-18 14:26:51 +0800

    解决bug 60960

--- src/pages/shimo/fileBaseline/iterateList/components/GridList/gridComponents/BaseLineStatus/index.tsx
+++ src/pages/shimo/fileBaseline/iterateList/components/GridList/gridComponents/BaseLineStatus/index.tsx
@@ -5,10 +5,10 @@ import "../style.less";
 
 const BaseLineStatus: React.FC<any> = (props: any) => {
   if (props.value === "未知" || props.value === "未基线") {
-    return (<Tag style={{color: "gray"}}>未基线</Tag>);
+    return (<Tag style={{color: "gray", width: 65, textAlign: "center"}}>未基线</Tag>);
   }
   if (props.value === "已基线") {
-    return (<Tag color={"green"}>已基线</Tag>);
+    return (<Tag style={{width: 65, textAlign: "center"}} color={"green"}>已基线</Tag>);
   }
   if (props.value === "部分基线") {
     return (<Tag color={"blue"}>部分基线</Tag>);

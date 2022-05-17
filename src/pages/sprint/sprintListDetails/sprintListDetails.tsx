d7d8e732a7bfb9d15c0d8d5d8eb90a94dab9c7a6
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-05-17 16:59:44 +0800

    修改时界面显示 2

--- src/pages/sprint/sprintListDetails/sprintListDetails.tsx
+++ src/pages/sprint/sprintListDetails/sprintListDetails.tsx
@@ -1029,7 +1029,7 @@ const SprintList: React.FC<any> = () => {
   const nessField = ['选择', '序号', '类型', '编号']; // 必需的列
   const unNessField = ['阶段', '测试', '测试确认', '标题内容', '创建时间', '解决时间', '所属计划', '严重等级', '截止日期', '模块', '状态', '已提测', '发布环境',
     '指派给', '解决/完成人', '关闭人', '备注', '相关需求', '相关任务', '相关bug', "是否涉及页面调整", '是否可热更', '是否有数据升级',
-    '是否有接口升级', '是否有预置数据', '是否需要测试验证', '验证范围建议', 'UED', 'UED测试环境验证', 'UED线上验证', '来源', '反馈人'];
+    '是否有接口升级', '是否有预置数据修改', '是否需要测试验证', '验证范围建议', 'UED', 'UED测试环境验证', 'UED线上验证', '来源', '反馈人'];
 
   const onSetFieldsChange = (checkedValues: any) => {
     setSelectedFiled(checkedValues);
@@ -2419,11 +2419,12 @@ const SprintList: React.FC<any> = () => {
         onCancel={fieldCancel}
         centered={true}
         footer={null}
-        width={920}
+        width={940}
       >
         <Form>
           <div>
-            <Checkbox.Group style={{width: '100%'}} value={selectedFiled} onChange={onSetFieldsChange}>
+            <Checkbox.Group style={{width: '110%',marginLeft:-10}} value={selectedFiled}
+                            onChange={onSetFieldsChange}>
               <Row>
                 <Col span={4}>
                   <Checkbox defaultChecked value="序号">序号</Checkbox>

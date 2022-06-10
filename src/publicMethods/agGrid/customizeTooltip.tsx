d29611b2843157c45c0d2e4bc01e8e6fcb1d94b0
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-06-10 18:20:30 +0800

    新增项目指标 2

---
+++ src/publicMethods/agGrid/customizeTooltip.tsx
@@ -0,0 +1,24 @@
+
+function CustomTooltip() {
+
+}
+
+CustomTooltip.prototype.init = function (params: any) {
+  // eslint-disable-next-line no-multi-assign
+  const eGui = (this.eGui = document.createElement('div'));
+
+  eGui.classList.add('custom-tooltip');
+
+  if (params.location === 'setFilterValue') {
+    eGui.innerHTML = `<strong>Full value:</strong> ${params.value}`;
+  } else {
+    eGui.innerHTML = params.value;
+  }
+};
+
+CustomTooltip.prototype.getGui = function () {
+  return this.eGui;
+};
+
+
+export {CustomTooltip};

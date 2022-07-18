d01b4f17e607288c0692b84f80d4b5247c73beed
wuxiaofeng <xiaofeng.wu@77hub.com>
2022-07-18 11:56:51 +0800

    发布公告接口调用

--- src/pages/onDutyAndRelease/releaseAnnouncement/axiosRequest/apiPage.tsx
+++ src/pages/onDutyAndRelease/releaseAnnouncement/axiosRequest/apiPage.tsx
@@ -1,32 +1,39 @@
-import {axiosGet,axiosGet_TJ, axiosPost} from "@/publicMethods/axios";
+import {axiosGet_TJ, axiosPost} from "@/publicMethods/axios";
 import {getCurrentUserInfo} from "@/publicMethods/authorityJudge";
+import dayjs from "dayjs";
 
 const users = getCurrentUserInfo();
 
 
 //根据编号获取公告内容
 export const getAnnouncement = async (readyReleaseNum: string, releaseType: string) => {
-debugger;
+
   const data = {
     ready_release_num: readyReleaseNum,
-    releaseType: releaseType
+    announcement_time: releaseType
   }
   const result = await axiosGet_TJ("/api/verify/release/announcement", data);
   return result;
 };
 
 // 发送（保存）公告
-export const postAnnouncement = async (formData: any, type: string) => {
+export const postAnnouncement = async (formData: any, basicInfo: any) => {
+
+  const upgradeTime = dayjs(formData.announceTime).format("YYYY-MM-DD HH:mm:ss");
   const data: any = {
-    "user_name": "string",
-    "user_id": "string",
-    "announcement_id": 0,
-    "ready_release_num": "string",
-    "upgrade_time": "string",
-    "upgrade_description": "string",
-    "is_upgrade": "yes",
-    "announcement_status": type
+    "user_name": users.name,
+    "user_id": users.userid,
+    "ready_release_num": basicInfo.releaseNum,
+    "upgrade_time": upgradeTime,
+    "upgrade_description": `${formData.announceDetails_1}${upgradeTime}${formData.announceDetails_2}`,
+    "is_upgrade": formData.showUpdateDetails === "true" ? "yes" : "no",
+    "announcement_status": basicInfo.releaseType,
+    "announcement_time": basicInfo.releaseTime
   };
+
+  if (formData.announcementId > 0) {
+    data.announcement_id = formData.announcementId;
+  }
   return axiosPost("/api/verify/release/announcement", data);
 };
 
--- src/pages/onDutyAndRelease/releaseAnnouncement/index.tsx
+++ src/pages/onDutyAndRelease/releaseAnnouncement/index.tsx
@@ -1,35 +1,59 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {PageContainer} from '@ant-design/pro-layout';
-import {errorMessage} from '@/publicMethods/showMessages';
+import {errorMessage, sucMessage} from '@/publicMethods/showMessages';
 import {Button, DatePicker, Form, Input, Radio, Tabs} from 'antd';
 import './style.css';
 import {useRequest} from 'ahooks';
 import {postAnnouncement, getAnnouncement} from "./axiosRequest/apiPage";
 import moment from "moment";
 
-
 const {TextArea} = Input;
 const {TabPane} = Tabs;
+let releaseTime = "before"; // 升级前公告还是升级后公告
 const Announce: React.FC<any> = (props: any) => {
   const releaseNum = props.location?.query?.releaseNum;
   const [announceContent] = Form.useForm();
-  const [formDatas, setFormDatas] = useState({
-    announceTime: "",
-    announceDetails_1: "亲爱的用户：您好，企企经营管理平台将于",
-    announceDetails_2: "",
-    showUpdateDetails: "true"
-  });
+  const [formDatas, setFormDatas] = useState({});
 
   const announceData = useRequest(() => getAnnouncement(releaseNum, "before")).data; // 关联值班名单
 
+  // 展示界面数据
+  const showFormData = (resData: any) => {
+    //   有数据的时候需要显示在界面上
+    if (resData.code === 4001) { // 没有发布公告，需要显示默认信息。
+      setFormDatas({
+        announcementId: 0,
+        announceTime: moment().add(1, 'day').startOf('day').format("YYYY-MM-DD HH:mm:ss"),
+        announceDetails_1: "亲爱的用户：您好，企企经营管理平台将于",
+        announceDetails_2: "",
+        showUpdateDetails: "true"
+      });
+
+    } else if (resData.data) { // 有数据，则展示出来
+      const {data} = resData;
+      const time = data.upgrade_time;
+      const details = (data.upgrade_description).split(time);
+      setFormDatas({
+        announcementId: data.announcement_id,
+        announceTime: time,
+        announceDetails_1: details[0],
+        announceDetails_2: details[1],
+        showUpdateDetails: data.is_upgrade === "yes" ? "true" : "false"
+      });
+    }
+  };
+
   // Tab 修改
   const onTabChanged = async (activeKey: string) => {
-    debugger;
-    // const announceContent = await getAnnouncement(releaseNum, activeKey);
 
+    releaseTime = activeKey;
+    const announceContent = await getAnnouncement(releaseNum, activeKey);
+    debugger;
+    showFormData(announceContent);
   }
+
   // 当表单种数据被改变时候
-  const whenFormvalueChanged = (changedFields: any, allFields: any) => {
+  const whenFormValueChanged = (changedFields: any, allFields: any) => {
     if (changedFields && changedFields.length > 0) {
       const fieldName = (changedFields[0].name)[0];
       let fieldValue = changedFields[0].value;
@@ -66,21 +90,21 @@ const Announce: React.FC<any> = (props: any) => {
   };
 
   // 点击保存或者发布按钮
-  const saveAndReleaseAnnouncement = (type: string) => {
-    postAnnouncement("", type);
+  const saveAndReleaseAnnouncement = async (releaseType: string) => {
+    const basicInfo = {releaseNum, releaseType, releaseTime}
+    const result = await postAnnouncement(formDatas, basicInfo);
+    const operate = releaseType === "save" ? "保存" : "公告挂起";
+    if (result.code === 200) {
+      sucMessage(`${operate}成功！`);
+    } else {
+      errorMessage(`${operate}失败！`);
+    }
   };
 
   useEffect(() => {
     if (announceData) {
-      //   有数据的时候需要显示在界面上
-      if (announceData.code === 4001) { // 没有发布公告，需要显示默认信息。
-        debugger
-      } else if (announceData.data) { // 有数据，则展示出来
-
-      }
+      showFormData(announceData);
     }
-
-
   }, [announceData])
   return (
     <PageContainer>
@@ -95,21 +119,21 @@ const Announce: React.FC<any> = (props: any) => {
 
         {/* Tab内容 */}
         <div style={{backgroundColor: "white", height: "700px", marginTop: -15}}>
-          <Form form={announceContent} autoComplete="off" onFieldsChange={whenFormvalueChanged}>
+          <Form form={announceContent} autoComplete="off" onFieldsChange={whenFormValueChanged}>
             <Form.Item label="升级时间:" name="announceTime" style={{paddingTop: 5}}>
-              <DatePicker defaultValue={moment()} showTime/>
+              <DatePicker value={moment(formDatas.announceTime)} showTime/>
             </Form.Item>
             <Form.Item label="公告详情:" name="announceDetails_1" style={{marginTop: -20}}>
-              <Input defaultValue={formDatas.announceDetails_1}/>
+              <Input value={formDatas.announceDetails_1}/>
             </Form.Item>
             <Form.Item name="showAnnounceTime" className={"marginStyle"}>
               <label style={{color: "gray"}}>{formDatas.announceTime}</label>
             </Form.Item>
             <Form.Item name="announceDetails_2" className={"marginStyle"}>
-              <TextArea rows={2}/>
+              <TextArea value={formDatas.announceDetails_2} rows={2}/>
             </Form.Item>
             <Form.Item label="展示查看更新详情:" name="showUpdateDetails" style={{marginTop: -20}}>
-              <Radio.Group defaultValue={formDatas.showUpdateDetails}>
+              <Radio.Group value={formDatas.showUpdateDetails}>
                 <Radio value={"true"}>是</Radio>
                 <Radio value={"false"}>否</Radio>
               </Radio.Group>
@@ -124,7 +148,7 @@ const Announce: React.FC<any> = (props: any) => {
                 <p>{"{"}</p>
                 <p className={"preview"}>"UpgradeIntroDate":"{formDatas.announceTime}"</p>
                 <p
-                  className={"preview"}>"UpgradeDescription":"{`${formDatas.announceDetails_1}${formDatas.announceDetails_2}`}"</p>
+                  className={"preview"}>"UpgradeDescription":"{`${formDatas.announceDetails_1}${formDatas.announceTime}${formDatas.announceDetails_2}`}"</p>
                 <p className={"preview"}> "isUpdated"：{formDatas.showUpdateDetails}</p>
                 <p>{"}"}</p>
               </div>

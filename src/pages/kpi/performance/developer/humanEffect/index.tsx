0bd10ab03e022beef37f59fed07a0e2b4994219d
liuqing <qing.liu@77hub.com>
2022-10-11 15:35:49 +0800

    增加数据默认保留两位小数

--- src/pages/kpi/performance/developer/humanEffect/index.tsx
+++ src/pages/kpi/performance/developer/humanEffect/index.tsx
@@ -30,6 +30,7 @@ const HumanEffect: React.FC<any> = () => {
       request={StatisticServices.humanEffect}
       identity={'DEVELOPER'}
       showDenominator={true}
+      len={5}
     />
   );
 };

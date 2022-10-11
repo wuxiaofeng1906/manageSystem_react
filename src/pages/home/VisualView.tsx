cf92b6950fdd88538322dfe8f90e7e477b3e9fcc
liuqing <qing.liu@77hub.com>
2022-10-11 15:10:18 +0800

    delete console

--- src/pages/home/VisualView.tsx
+++ src/pages/home/VisualView.tsx
@@ -259,9 +259,6 @@ const VisualView = () => {
       setLoading(false);
     }
   };
-  let te = 'cn-northwest-2345';
-  const text = te.replace('cn-northwest-', '集群');
-  console.log(text.length > 3 ? `${text.slice(0, 3)}-${text.slice(text.length - 1)}` : text);
 
   const onlineLen = useMemo(() => online.length, [online]);
   // 动态列

68e6ce5d06c7efe65a8565902b25fb395a8e2665
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-03-08 16:44:21 +0800

    当轮播页减少，判断其修改操作

--- src/pages/onlineSystem/announcement/announcementDetail/axiosRequest/apiPageForUpdate.tsx
+++ src/pages/onlineSystem/announcement/announcementDetail/axiosRequest/apiPageForUpdate.tsx
@@ -68,7 +68,7 @@ const carouselDataForAdd = (popupData: any) => {
 };
 
 // 更新公告内容
-export const updateAnnounceContentForAdd = async (formData: any, popupData: object = {}) => {
+const updateAnnounceContentForAdd = async (formData: any, popupData: object = {}) => {
   const data: any = {
     iteration: formData.announce_name, // 公告名称：默认带入当前时间，可修改，必填(string)
     templateTypeId: formData.modules, // 通知模板：1.消息卡片，2.弹窗
@@ -456,34 +456,86 @@ const firstSpecialDataUpdate = (newCommonData: any, modifyPopData: any, oldCommo
   return pages;
 };
 
+const addCarousePage = (oldCommonData: any, newCommonData: any, newPopData: any) => {
+
+  // 新增轮播页
+  let page: any = [];
+  //  拿最后新增的
+  for (let i = 0; i < newPopData.length; i++) {
+    const pageDt = newPopData[i].tabsContent;
+    const content: any = [];
+
+    (pageDt.ptyGroup).map((v: any) => {
+
+      // 二级特性
+      const secondLevel: any = [];
+      const second = v.seconds;
+      second.map((v2: any) => {
+        if (v2.first) {
+          secondLevel.push({
+            speciality: v2.first,
+            editFlag: "add"
+          })
+        }
+      });
+      content.push({
+        speciality: v.first,
+        editFlag: "add",
+        children: secondLevel
+      });
+    });
+    page.push({
+      image: pageDt.uploadPic,
+      pageNum: newPopData[i].tabPage,
+      layoutTypeId: pageDt.picLayout,
+      editFlag: "add",
+      contents: [{
+        speciality: pageDt.specialName,
+        editFlag: "add",
+        children: content
+      }]
+    })
+  }
+  return page;
+}
+
 // 轮播页发生改变
 const carousePageUpdate = (newCommonData: any, newPopData: any, oldCommonData: any, oldPopData: any) => {
-
   let page: any = [];
-  // if (oldCommonData.carouselNum > newCommonData.carouselNum) {
-  //  轮播页减少,删除最后几张的轮播页数(特性可能会被修改)
-  let modifyPopData = []
-  for (let i = 0; i < oldCommonData.carouselNum; i++) {
-    const pageDt = newPopData[i].tabsContent;
-    if (i > newCommonData.carouselNum - 1 && pageDt.id) {
-      // 需要删除的page
-      page.push({
-        id: pageDt.id,
-        editFlag: "delete",
-      })
-    } else {
-      modifyPopData.push(newPopData[i]);
+  //  轮播页减少,1.删除最后几张的轮播页数(特性可能会被修改) 2.删除所有的页面重建
+  if (newCommonData.clearTabContent) {
+    // 清空重建旧数据全部用delete，新数据全部用add
+    // 需要删除的page
+    oldPopData.map((v: any) => {
+      if (v.tabsContent?.id) {
+        page.push({
+          id: v.tabsContent?.id,
+          editFlag: "delete",
+        });
+      }
+    });
+
+    //这个是新增的数据
+    page = page.concat(addCarousePage(oldCommonData, newCommonData, newPopData));
+  } else {
+    let modifyPopData = []
+    for (let i = 0; i < oldCommonData.carouselNum; i++) {
+      const pageDt = newPopData[i].tabsContent;
+      if (i > newCommonData.carouselNum - 1 && pageDt.id) {
+        // 需要删除的page
+        page.push({
+          id: pageDt.id,
+          editFlag: "delete",
+        });
+      } else {
+        modifyPopData.push(newPopData[i]);
+      }
+    }
+    if (modifyPopData.length > 0) {
+      const specialItem = firstSpecialDataUpdate(newCommonData, modifyPopData, oldCommonData, oldPopData);
+      page = page.concat(specialItem);
     }
   }
-
-  if (modifyPopData.length > 0) {
-    // 判断一级特性是否修改过:是否可以不进行改变判断，直接取获取数据
-    // if (firstSpecialIsUpdate(newCommonData, modifyPopData, oldPopData)) {
-    const specialItem = firstSpecialDataUpdate(newCommonData, modifyPopData, oldCommonData, oldPopData);
-    page = page.concat(specialItem);
-    // }
-  }
-  debugger;
   return {
     id: oldCommonData.id,
     pageSize: newCommonData.carouselNum,
@@ -492,65 +544,11 @@ const carousePageUpdate = (newCommonData: any, newPopData: any, oldCommonData: a
     description: newCommonData.announce_content,
     pages: page
   }
-  //
-  // } else if (oldCommonData.carouselNum < newCommonData.carouselNum) {
-  //   // 新增轮播页
-  //   let page: any = [];
-  //   //  拿最后新增的
-  //   for (let i = oldCommonData.carouselNum; i < newCommonData.carouselNum; i++) {
-  //     const pageDt = newPopData[i].tabsContent;
-  //     const content: any = [];
-  //
-  //     (pageDt.ptyGroup).map((v: any) => {
-  //
-  //       // 二级特性
-  //       const secondLevel: any = [];
-  //       const second = v.seconds;
-  //       second.map((v2: any) => {
-  //         if (v2.first) {
-  //           secondLevel.push({
-  //             speciality: v2.first,
-  //             editFlag: "add"
-  //           })
-  //         }
-  //       });
-  //       content.push({
-  //         speciality: v.first,
-  //         editFlag: "add",
-  //         children: secondLevel
-  //       });
-  //     });
-  //     page.push({
-  //       image: pageDt.uploadPic,
-  //       pageNum: newPopData[i].tabPage,
-  //       layoutTypeId: pageDt.picLayout,
-  //       editFlag: "add",
-  //       contents: [{
-  //         speciality: pageDt.specialName,
-  //         editFlag: "add",
-  //         children: content
-  //       }]
-  //     })
-  //   }
-  //   return {
-  //     id: oldCommonData.id,
-  //     pageSize: newCommonData.carouselNum,
-  //     iteration: newCommonData.announce_name,
-  //     updatedTime: dayjs(newCommonData.announce_time).format("YYYY-MM-DD hh:mm:ss"),
-  //     description: newCommonData.announce_content,
-  //     // 模板类型和是否轮播不能修改
-  //     // "templateTypeId": newCommonData.modules,
-  //     // "isCarousel": true,
-  //     pages: page
-  //   }
-  // }
-  // return {}
 };
 
 
 // 修改发布公告
 export const updateAnnouncement = async (releaseID: string, newCommonData: any, newPopData: any) => {
-
   // 构造基础数据
   let oldCommonData = {
     id: releaseID,
@@ -594,7 +592,7 @@ export const updateAnnouncement = async (releaseID: string, newCommonData: any, 
     const carouseNumState = carouseNumIsUpdate(newCommonData, oldCommonData);  // 轮播页数是否修改
     const firstLevelState = firstSpecialIsUpdate(newCommonData, newPopData, oldPopData); // 一级特性是否增加或者删除
     const secondLevelState = secondSpecialIsUpdate(newCommonData, newPopData, oldPopData); // 二级特性是否增加或者删除
-    // 如果只是修改过轮播页数，一级特性和二级特性都没有被修改过
+    //  轮播页数，一级特性和二级特性被修改过
     if (carouseNumState || firstLevelState || secondLevelState || specialNameState) {
       relData = carousePageUpdate(newCommonData, newPopData, oldCommonData, oldPopData);
     } else {

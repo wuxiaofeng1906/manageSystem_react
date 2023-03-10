28f4a46d7d47eb9ea2ac2f7c33609a8b493df17a
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-03-10 15:02:14 +0800

    当一级特性中只有一个二级特性时不能进行删除

--- src/pages/onlineSystem/announcement/announcementDetail/PopupCard/index.tsx
+++ src/pages/onlineSystem/announcement/announcementDetail/PopupCard/index.tsx
@@ -374,7 +374,15 @@ const PopupCard: React.FC<any> = (props: any) => {
                                       <Button
                                         style={{border: 'none', color: "red", marginLeft: 5}}
                                         icon={<MinusCircleOutlined/>}
-                                        onClick={() => removeSeond(secondField.name)}
+                                        onClick={() => {
+                                          debugger
+                                          // 仅有一个二级属性时不能删
+                                          if (!secondFields || secondFields.length <= 1) {
+                                            errorMessage("只有一个二级特性时不能删除！")
+                                            return;
+                                          }
+                                          removeSeond(secondField.name);
+                                        }}
                                       />
                                     </Row>
                                   </div>

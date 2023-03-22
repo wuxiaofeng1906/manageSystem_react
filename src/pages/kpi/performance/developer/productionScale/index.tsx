9e9a79f34858a83aa6479be59a9a97beea60f531
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-03-22 17:54:06 +0800

    开发生产规模 -规则展示修改

--- src/pages/kpi/performance/developer/productionScale/index.tsx
+++ src/pages/kpi/performance/developer/productionScale/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import IStaticAgTable, { IRuleData } from '@/components/IStaticAgTable';
+import IStaticAgTable, {IRuleData} from '@/components/IStaticAgTable';
 import StatisticServices from '@/services/statistic';
+
 const ruleData: IRuleData[] = [
   {
     title: '统计范围',
     child: [
       '需求规模：zt_story.scale != 0',
-      '统计在该周期完成的需求规模（需求关联的任务由开发完成/关闭，取任务的完成/关闭日期落在该周期的）',
-      <p style={{ textIndent: '3em', margin: '5px 0' }}>
-        任务范围：关联需求的任务状态为已完成/已关闭且是由开发完成/关闭的，关联需求的任务状态为未开始/进行中且当前指派给为开发的;
+      '统计在该周期完成的规模（任务由开发完成/关闭，取任务的完成/关闭日期落在该周期的）',
+      <p style={{textIndent: '3em', margin: '5px 0'}}>
+        任务范围：任务状态为已完成/已关闭且是由开发完成/关闭的，任务状态为未开始/进行中且当前指派给为开发的；
       </p>,
-      <p style={{ textIndent: '3em', margin: '5px 0' }}>
-        任务规模 = 该人员在该周期完成该需求的开发任务工时/该需求开发总工时*该需求规模开发任务总工时=
-        被开发完成/关闭任务取实际消耗工时+进行中任务工时为（实际消耗+剩余工时） +
-        未开始任务取预计工时;
+      <p style={{textIndent: '3em', margin: '5px 0'}}>
+        任务规模 = 该开发人员在该周期完成的任务总工时/该周期开发总工时*该开发在该周期投入所有项目的规模之和*
+        （该周期开发总工时/截止目前项目开发总工时）；
       </p>,
     ],
   },

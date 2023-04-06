c3371e3332682babb18b5d2b685dd2d26436d68a
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-04-06 14:15:30 +0800

    添加antd消息提示封装接口

--- src/publicMethods/showMessages.tsx
+++ src/publicMethods/showMessages.tsx
@@ -1,5 +1,47 @@
 import {message} from "antd";
 
+
+interface Iparam {
+  type: string; // 提示类型
+  msg: string; // 展示信息
+  dutationTime?: number; // 展示的时间长度
+  position?: string; // 展示的位置：50vh 是居中展示
+
+}
+
+// 自定义错误信息展示
+export const customMessage = ({type = "info", msg = "", dutationTime = 1.5, position = '50vh'}: Iparam) => {
+  const msgConfig = {
+    content: msg,
+    duration: dutationTime,
+    style: {
+      marginTop: position,
+    },
+  }
+
+  switch (type) {
+    case "error":
+      message.error(msgConfig);
+      break;
+
+    case "info":
+      message.info(msgConfig);
+      break;
+
+    case "success":
+      message.success(msgConfig);
+      break;
+
+    case "warn":
+      message.warning(msgConfig);
+      break;
+
+    default:
+      break;
+  }
+};
+
+
 // 错误信息
 const errorMessage = (errMsg: string, dutationTime: number = 1.5, position: string = '50vh') => {
   message.error({

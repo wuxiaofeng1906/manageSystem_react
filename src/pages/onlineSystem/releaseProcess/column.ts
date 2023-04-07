8ffc543281eef9ff9b6c80ed3fdda32b77f95a0d
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-04-07 17:34:59 +0800

    界面展示取消发布

--- src/pages/onlineSystem/releaseProcess/column.ts
+++ src/pages/onlineSystem/releaseProcess/column.ts
@@ -59,8 +59,10 @@ export const releaseListColumn = (type: 'history' | 'pre'): (ColDef | ColGroupDe
     hide: type == 'pre',
     cellRenderer: (p: any) =>
       `<span style="color:${
-        p.value == 'success' ? '#2BF541' : p.value == 'failure' ? 'red' : 'initial'
-      }">${p.value == 'success' ? '发布成功' : p.value == 'failure' ? '发布失败' : ''}</span>`,
+        p.value == 'success' ? '#2BF541' : p.value == 'failure' ? 'red' : p.value === "unknown" ? "gray" : 'initial'
+      }">${
+        p.value == 'success' ? '发布成功' : p.value == 'failure' ? '发布失败' : p.value === "unknown" ? "取消发布" : ''
+      }</span>`,
   },
   {
     headerName: '发布方式',

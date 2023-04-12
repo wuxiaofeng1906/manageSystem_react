90a7565a662a4f732e21600d050e0036f190fbcd
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-04-12 11:02:16 +0800

    发布过程公告选择接口参数匹配

--- src/services/announcement.ts
+++ src/services/announcement.ts
@@ -1,22 +1,24 @@
 import request from './request';
+
 const baseUrl = '/api/verify';
 // 发布过程
 const AnnouncementServices = {
   // 公告列表
   async announcementList(params: any) {
-    return request(`${baseUrl}/release/announcement_list`, { params });
+    return request(`${baseUrl}/release/announcement_list`, {params});
   },
   // 发布过程 公告列表
   async preAnnouncement() {
-    return request(`${baseUrl}/release/announcement_name`);
+    debugger
+    return request(`${baseUrl}/release/announcement_name`, {params: {size: 50}});
   },
   // 发布过程 公告关联
   async preReleaseAssociation(data: any) {
-    return request(`${baseUrl}/release/ready_announcement_num`, { data, method: 'post' });
+    return request(`${baseUrl}/release/ready_announcement_num`, {data, method: 'post'});
   },
   // 发布历史 公告关联
   async releaseHistoryAssociation(data: any) {
-    return request(`${baseUrl}/release/release_announcement_num`, { data, method: 'post' });
+    return request(`${baseUrl}/release/release_announcement_num`, {data, method: 'post'});
   },
   // 所有人员
   async applicant() {
@@ -34,7 +36,7 @@ const AnnouncementServices = {
   // 删除验证是否存在关联公告
   async checkDeleteAnnouncement(announcement_num: string) {
     return request(`${baseUrl}/release/release_announcement_num`, {
-      params: { announcement_num },
+      params: {announcement_num},
     });
   },
 };

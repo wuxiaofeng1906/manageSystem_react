edb35ae91f15ea1b06e9f55b4ba5b10230cc5d5a
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-04-20 16:25:09 +0800

    添加历史记录的tab标签展示 4

--- src/pages/onlineSystem/components/ProcessTabs.tsx
+++ src/pages/onlineSystem/components/ProcessTabs.tsx
@@ -3,19 +3,17 @@ import React, {useEffect, useState} from "react";
 import PreReleaseServices from "@/services/preRelease";
 import {useLocation, useParams} from "umi";
 import {history} from "@@/core/history";
-import {useModel} from "@@/plugin-model/useModel";
 
 export const ProcessTabs = ({finished}: { finished: boolean }) => {
   // tab的数据
   const [list, setList] = useState<any>([]);
   const {release_num, release_name} = useParams() as { release_num: string; release_name: string }; // 非积压发布获取参数
   const {id} = useParams() as { id: string; }; // 灰度推线上获取参数
-  const {globalState} = useModel('onlineSystem');
 
   // 切换tab
   const onTabChange = (v: any) => {
     // 跳转链接需要找到下一个数据的subtab和tab
-    let p = list.find((e: any) => {
+    const p = list.find((e: any) => {
       return e.release_num === v
     });
 
@@ -29,11 +27,15 @@ export const ProcessTabs = ({finished}: { finished: boolean }) => {
   //获取发布列表
   const getTabsList = async () => {
     // effect 中调用异步信息时间延迟可能导致最终获取的数据不正确。
-    debugger
     let tabList = await PreReleaseServices.releaseList();
     // 如果是历史记录，则只展示一个Tab
     if (finished) {
-      tabList = [{release_num, release_name}];
+      const path = history.location.pathname;
+      if (path.includes("/onlineSystem/releaseOrder/")) { //  releaseOrder 的工单编号不是release_num
+        tabList = [{release_num: id, release_name}];
+      } else {
+        tabList = [{release_num, release_name}];
+      }
     }
 
     setList(tabList);

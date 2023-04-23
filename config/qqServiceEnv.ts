422264b075f41d0a36df77ef12e69c35a235c89d
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-04-23 18:30:32 +0800

    修改正式环境地地址 2

--- config/qqServiceEnv.ts
+++ config/qqServiceEnv.ts
@@ -4,7 +4,7 @@ export const Notice_Common_Test = `http://identity.${Notice_Env_Test}-global.e7l
 
 // 正式环境
 export const Notice_Env = "cn-northwest-global"; // 传给后端的环境（正式环境）
-export const Notice_Common = `http://identity.${Notice_Env}.77hub.com/graphiql/index.html`;   // 上传图片之前的鉴权获取，以及公告GQL查询（正式环境）
+export const Notice_Common = `http://identity.${Notice_Env}.77hub.com/`;   // 上传图片之前的鉴权获取，以及公告GQL查询（正式环境）
 
 export const Notice_ImageView = "http://s3.cn-northwest-1.amazonaws.com.cn/cn-northwest-1-q7link-test/"; // 图片上传后，通过这个链接拼接上传后的图片ID就可以访问到图片。
 export const Notice_PostImage = "http://s3.cn-northwest-1.amazonaws.com.cn/";   // 将选中的图片发送到该地址对应的服务器

9dafa85d94686de118b29bea91f7fa67ab38ef41
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-06-07 16:09:57 +0800

    还原以前的请求数据

--- src/services/request.ts
+++ src/services/request.ts
@@ -107,13 +107,13 @@ function dealResWrap(mRequest: Promise<any>, warn: any, forceLogin: boolean, msg
     })
     .catch((e) => {
 
-      if (e.status) {
-        errorMessage(`请求异常信息:${e.status.toString()} ${e.statusText.toString()}`);
-      } else if (e.code) {
-        errorMessage(`请求异常信息:${e.code.toString()} ${e.msg.toString()}`);
-      } else {
-        errorMessage(`请求异常!`);
-      }
+      // if (e.status) {
+      //   errorMessage(`请求异常信息:${e.status.toString()} ${e.statusText.toString()}`);
+      // } else if (e.code) {
+      //   errorMessage(`请求异常信息:${e.code.toString()} ${e.msg.toString()}`);
+      // } else {
+      //   errorMessage(`请求异常!`);
+      // }
 
       return Promise.reject(e);
     });

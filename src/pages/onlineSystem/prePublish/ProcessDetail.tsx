bd32ebc5c5ccc39b7fd1993616935851c9f8cdf1
wuxiaofeng <xiaofeng.wu@77hub.com>
2023-07-25 15:58:50 +0800

    查看明细时候的下拉框判定 3

--- src/pages/onlineSystem/prePublish/ProcessDetail.tsx
+++ src/pages/onlineSystem/prePublish/ProcessDetail.tsx
@@ -145,7 +145,6 @@ const ProcessDetail = (props: any, ref: any) => {
 
   useEffect(() => {
     if (isEmpty(basic?.branch) || subTab !== 'server') return;
-debugger
     showInitBranchEnv(basic?.release_env_type, basic?.branch);
 
   }, [basic, subTab]);
